35fa315136a3e81d51280ee0000ef32836a734ef
Eli Jhonathan <106014593+elijhonathan@users.noreply.github.com>
2023-12-20 21:11:07 -0300

    Add files via upload

---
+++ sites.js
@@ -0,0 +1,40 @@
+
+function Somar(num1, num2)
+{
+    const somar = num1 + num2
+    return somar
+}
+
+function Subtrair(num1, num2)
+{
+    const subtrair = num1 - num2
+    return subtrair
+}
+
+
+function Dividir(num1, num2)
+{
+    const dividir = num1 / num2
+    return dividir
+}
+
+
+function Multiplicar(num1, num2)
+{
+    const multiplicar = num1 * num2
+    return multiplicar
+}
+
+
+
+
+let soma = Somar(1,2);
+let reduzir = Subtrair(7,3);
+let dividir = Dividir(10,5)
+let multiplicar = Multiplicar(100,10)
+
+console.log(soma)
+console.log(reduzir)
+console.log(dividir)
+console.log(multiplicar)
+
